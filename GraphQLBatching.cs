9b3ab1f321cc65c7a3b391ed00ab4c223fde6c9b
UrielBatista <49211381+UrielBatista@users.noreply.github.com>
2023-03-28 15:48:31 -0700

    Add files via upload

---
+++ GraphQLBatching.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace GraphQLBatchingResponse
+{
+    public class GraphQLBatching
+    {
+        public static T[] GraphQLBatchingMapper<T>(string data, string key)
+        {
+            List<T> arrayElement = new List<T>();
+            var treatedElements = data.Replace("event: next", "")
+                .Trim().Replace("data:", "")
+                .Trim().Replace("event: complete", "")
+                .Trim().Split(new string[] { "\n\n\n " }, StringSplitOptions.None);
+
+            foreach (var element in treatedElements)
+            {
+                try
+                {
+                    var elementParsed = JsonDocument.Parse(element);
+                    var rootElement = elementParsed.RootElement
+                    .GetProperty("data")
+                    .GetProperty(key).ToString();
+
+                    var elementDeserialized = JsonSerializer.Deserialize<List<T>>(rootElement);
+                    arrayElement.AddRange(elementDeserialized);
+
+                }
+                catch (Exception ex)
+                {
+                    _ = ex;
+                    continue;
+                }
+            }
+
+            return arrayElement.ToArray();
+        }
+    }
+}
