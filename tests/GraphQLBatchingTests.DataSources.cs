18607eca89591a0329c9fb80cf1ac45525506997
UrielBatista <49211381+UrielBatista@users.noreply.github.com>
2023-03-31 22:49:34 -0300

    Add files via upload

---
+++ tests/GraphQLBatchingTests.DataSources.cs
@@ -0,0 +1,46 @@
+﻿using UnitTestsPackage.Dtos;
+
+namespace UnitTestsPackage
+{
+    public sealed partial class GraphQLBatchingTests
+    {
+        private static class DataSouces
+        {
+            public static IEnumerable<object[]> GetOneItemOrNullEmptyData { get; } = new[]
+            {
+                new object[]
+                {
+                    "",
+                    "items",
+                    new List<ItemsDataDto>
+                    {
+                    },
+                },
+                new object[]
+                {
+                    "data: {\"not_items\": [] } event: complete\n\n\n ",
+                    "items",
+                    new List<ItemsDataDto>
+                    {
+                        new ItemsDataDto
+                        {
+                            id_Items = string.Empty,
+                        },
+                    },
+                },
+                new object[]
+                {
+                    "event: next\ndata: {\"data\":{\"itemsData\":[{\"id_Items\":\"01\"}]}}\n\nevent: complete\n\n",
+                    "itemsData",
+                    new List<ItemsDataDto>
+                    {
+                        new ItemsDataDto
+                        {
+                            id_Items = "01",
+                        },
+                    },
+                }
+            };
+        }
+    }
+}
